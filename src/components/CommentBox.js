c509e4bb6e8b3ae11ff5a2680a0d80347d8e6a1e
BayLadyCoder <rachada_32@hotmail.com>
2021-07-11 19:18:36 -0400

    Create form in CommentBox and handleChange and handleSubmit functions

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CommentBox = () => {
-  return <div>Comment Box</div>;
+  const [comment, setComment] = useState("");
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Call an action creator to save the comment
+    setComment("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Add a comment</h4>
+      <textarea
+        onChange={handleChange}
+        value={comment}
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+      />
+      <div>
+        <button type="submit">Submit Comment</button>
+      </div>
+    </form>
+  );
 };
 
 export default CommentBox;
--- src/components/__tests___/App.test.js
+++ src/components/__tests___/App.test.js
@@ -12,10 +12,10 @@ beforeEach(() => {
 });
 
 describe("App", () => {
-  it("shows a comment box", () => {
+  it("Shows a comment box", () => {
     expect(wrapper.find(CommentBox).length).toEqual(1);
   });
-  it("shows a comment list", () => {
+  it("Shows a comment list", () => {
     expect(wrapper.find(CommentList).length).toEqual(1);
   });
 });

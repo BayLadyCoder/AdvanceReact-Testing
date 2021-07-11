b4e87ddc8d6498baf862b157808906be38b7f8f9
BayLadyCoder <rachada_32@hotmail.com>
2021-07-11 18:40:50 -0400

    Add describe in test

--- src/components/__tests___/App.test.js
+++ src/components/__tests___/App.test.js
@@ -11,9 +11,11 @@ beforeEach(() => {
   wrapper = shallow(<App />);
 });
 
-it("shows a comment box", () => {
-  expect(wrapper.find(CommentBox).length).toEqual(1);
-});
-it("shows a comment list", () => {
-  expect(wrapper.find(CommentList).length).toEqual(1);
+describe("App", () => {
+  it("shows a comment box", () => {
+    expect(wrapper.find(CommentBox).length).toEqual(1);
+  });
+  it("shows a comment list", () => {
+    expect(wrapper.find(CommentList).length).toEqual(1);
+  });
 });

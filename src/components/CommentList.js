e0e9ffb7a0ab99e09481159cd6b4088d539c28ad
BayLadyCoder <rachada_32@hotmail.com>
2021-07-11 22:25:38 -0400

    Create CommentList Component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const CommentList = () => {
-  return <div>Comment List</div>;
+const CommentList = ({ comments }) => {
+  return (
+    <div>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment}>{comment}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default CommentList;
+const mapStateToProps = (state) => {
+  return {
+    comments: state.comments,
+  };
+};
+
+export default connect(mapStateToProps, null)(CommentList);

2cd342db3c9b2b06b8bc82d9f8700ef59ff8a337
BayLadyCoder <rachada_32@hotmail.com>
2021-07-11 21:08:41 -0400

    Create actionCreator for SAVE_COMMENT and add this case to commentReducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,7 +1,11 @@
-const commentsReducer = (state = {}, action) => {
+import { SAVE_COMMENT } from "actions/types";
+
+const commentsReducer = (state = [], action) => {
   const { type, payload } = action;
 
   switch (type) {
+    case SAVE_COMMENT:
+      return [...state, payload];
     default:
       return state;
   }
